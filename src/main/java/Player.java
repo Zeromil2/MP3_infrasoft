7c385a750275dec82093f869555676fe18b42024
PedroCLins <pedro.fev03@gmail.com>
2024-02-14 19:43:54 -0300

    Implementadas as demais funções para a entrega 1: - Remove; - Stop; - Play/Pause; - Play Now.

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -8,6 +8,7 @@ import javax.swing.event.MouseInputAdapter;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -30,26 +31,59 @@ public class Player {
     private String[][] infoDasMusicas = new String[0][];
     private ArrayList<Song> listaDeMusicas = new ArrayList<Song>();
     private int currentFrame = 0;
+    private int estaTocando = 0;     // 0 = Não está tocando; 1 = Está tocando;
+    private int indice; // = this.window.getSelectedSongIndex();
+    private Song musicaAtual; // = listaDeMusicas.get(indice);
+    private Thread threadDaMusica;
 
     private final ActionListener buttonListenerPlayNow = e -> {
-        currentFrame = 0;
+        iniciarNovaThread();
     };
     private final ActionListener buttonListenerRemove = e -> {
+        int idxMusicaSelec = this.window.getSelectedSongIndex();
+        int idxMusicaAtual = listaDeMusicas.indexOf(musicaAtual);
+        if (idxMusicaSelec == idxMusicaAtual) {
+            interromperThread(threadDaMusica, bitstream, device);
+            window.resetMiniPlayer();
+        }
+        listaDeMusicas.remove(listaDeMusicas.get(idxMusicaSelec));
+        int tamanho = infoDasMusicas.length;
+        if (idxMusicaSelec == 0) {
+            infoDasMusicas = Arrays.copyOfRange(infoDasMusicas, 1, tamanho);
+        } else if (idxMusicaSelec == tamanho-1) {
+            infoDasMusicas = Arrays.copyOfRange(infoDasMusicas, 0, tamanho-1);
+        } else {
+            String[][] parte1 = Arrays.copyOfRange(infoDasMusicas, 0, idxMusicaSelec);
+            String[][] parte2 = Arrays.copyOfRange(infoDasMusicas, idxMusicaSelec+1, tamanho);
+            infoDasMusicas = Arrays.copyOf(parte1, parte1.length + parte2.length);
+            System.arraycopy(parte2, 0, infoDasMusicas, parte1.length, parte2.length);
+        }
+        this.window.setQueueList(infoDasMusicas);
     };
     private final ActionListener buttonListenerAddSong = e -> {
         Song musica = this.window.openFileChooser();
         listaDeMusicas.add(musica);
-
         String[] dadosDaMusica = musica.getDisplayInfo();
         int tamanho = infoDasMusicas.length;
         infoDasMusicas = Arrays.copyOf(infoDasMusicas, tamanho+1);
         infoDasMusicas[tamanho] = dadosDaMusica;
-
         this.window.setQueueList(infoDasMusicas);
     };
 
-    private final ActionListener buttonListenerPlayPause = e -> {};
-    private final ActionListener buttonListenerStop = e -> {};
+    private final ActionListener buttonListenerPlayPause = e -> {
+        if (estaTocando == 1) {
+            estaTocando = 0;
+            window.setPlayPauseButtonIcon(0);
+        } else {
+            estaTocando = 1;
+            window.setPlayPauseButtonIcon(1);
+            novaThread();
+        }
+    };
+    private final ActionListener buttonListenerStop = e -> {
+        interromperThread(threadDaMusica, bitstream, device);
+        window.resetMiniPlayer();
+    };
     private final ActionListener buttonListenerNext = e -> {};
     private final ActionListener buttonListenerPrevious = e -> {};
     private final ActionListener buttonListenerShuffle = e -> {};
@@ -124,12 +158,63 @@ public class Player {
         while (framesToSkip-- > 0 && condition) condition = skipNextFrame();
     }
 
+    private void iniciarNovaThread() {
+        interromperThread(threadDaMusica, bitstream, device);   // Chama a função para interromper a thread atual
+        indice = this.window.getSelectedSongIndex();
+        musicaAtual = listaDeMusicas.get(indice);
+        criarObjetos();
+
+        // Reseta as variáveis da música, setando o frame atual para 0 e toquePause para o estado 1 (de tocar).
+        currentFrame = 0;
+        estaTocando = 1;
+        window.setPlayingSongInfo(musicaAtual.getTitle(), musicaAtual.getAlbum(), musicaAtual.getArtist());
+
+        novaThread();
+    }
+
+    private void novaThread() {
+        threadDaMusica = new Thread(() -> {
+            int tempoTotal = (int) (musicaAtual.getNumFrames() * musicaAtual.getMsPerFrame());
+            while (true) {
+                if (estaTocando == 1) {
+                    window.setTime((int) (currentFrame * musicaAtual.getMsPerFrame()), tempoTotal);
+                    window.setPlayPauseButtonIcon(estaTocando);
+                    window.setEnabledPlayPauseButton(true);
+                    window.setEnabledStopButton(true);
+                    try {
+                        if (!this.playNextFrame()) {
+                            estaTocando = 0;
+                            window.resetMiniPlayer();
+                        } else {
+                            currentFrame++;
+                        }
+                    } catch (JavaLayerException exception) {
+                        throw new RuntimeException();
+                    }
+                }
+            }
+        });
+        threadDaMusica.start();
+    }
+
+    private void interromperThread(Thread threadDaMusica, Bitstream bitstream, AudioDevice device) {
+        if (bitstream != null) {
+            threadDaMusica.interrupt();
+            try {
+                bitstream.close();
+                device.close();
+            } catch (BitstreamException exception) {
+                throw new RuntimeException();
+            }
+        }
+    }
+
     private void criarObjetos() {
         try {
             this.device = FactoryRegistry.systemRegistry().createAudioDevice();
             this.device.open(this.decoder = new Decoder());
             this.bitstream = new Bitstream(musicaAtual.getBufferedInputStream());
-        } catch (JavaLayerException exception) {
+        } catch (JavaLayerException | FileNotFoundException exception) {
             throw new RuntimeException();
         }
     }
